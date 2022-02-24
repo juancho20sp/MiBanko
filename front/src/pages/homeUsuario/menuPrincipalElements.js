1d2da43dcb81655158939fae8e8f83a98c996ec4
JCPosso <juancamiloposso01@gmail.com>
2022-02-24 00:28:43 -0500

    Add DashBoard Users page

---
+++ front/src/pages/homeUsuario/menuPrincipalElements.js
@@ -0,0 +1,136 @@
+import styled from  'styled-components';
+import { Link as LinkR } from 'react-router-dom';
+
+export const RootWrapperHomeUsuario = styled.div`
+min-height: 100vh;
+background-color: rgba(255, 255, 255, 1);
+position: relative;
+`;
+
+export const Rectangle5 = styled.svg`
+width: 644px;
+	height: 419px;
+	position: absolute;
+	left: 434px;
+	top: 355px;
+`;
+
+export const Rectangle5Stroke = styled.svg`
+width: 644px;
+height: 419px;
+position: absolute;
+left: 434px;
+top: 355px;
+`;
+
+export const MenúPrincipal = styled.span`
+color: rgba(0, 0, 0, 1);
+text-overflow: ellipsis;
+font-size: 60px;
+font-family: Roboto, sans-serif;
+font-weight: 400;
+text-align: left;
+position: absolute;
+left: 557px;
+top: 89px;
+`;
+
+export const TodoMiDinero123436 = styled.span`
+color: rgba(0, 0, 0, 1);
+text-overflow: ellipsis;
+font-size: 45px;
+font-family: Roboto, sans-serif;
+font-weight: 400;
+text-align: left;
+position: absolute;
+left: 499px;
+top: 230px;
+`;
+
+export const Rectangle3 = styled.div`
+width: 559px;
+height: 61px;
+background-color: rgba(113, 107, 107, 1);
+border-radius: 20px;
+position: absolute;
+left: 478px;
+top: 400px;
+`;
+
+export const Rectangle6 = styled.div`
+width: 559px;
+height: 61px;
+background-color: rgba(113, 107, 107, 1);
+border-radius: 20px;
+position: absolute;
+left: 477px;
+top: 499px;
+`;
+
+export const Rectangle7 = styled.div`
+width: 559px;
+height: 61px;
+background-color: rgba(113, 107, 107, 1);
+border-radius: 20px;
+position: absolute;
+left: 477px;
+top: 598px;
+`;
+
+
+export const Rectangle8 = styled.div`
+	width: 559px;
+	height: 61px;
+	background-color: rgba(113, 107, 107, 1);
+	border-radius: 20px;
+	position: absolute;
+	left: 478px;
+	top: 688px;
+`;
+export const EnviarDineroBtn  =styled(LinkR)`
+color: rgba(255, 255, 255, 1);
+	text-overflow: ellipsis;
+	font-size: 40px;
+	font-family: Roboto, sans-serif;
+	font-weight: 400;
+	text-align: left;
+	position: absolute;
+	left: 642px;
+	top: 407px;
+`;
+
+export const VerMisMovimientosBtn  =styled(LinkR)`
+color: rgba(255, 255, 255, 1);
+text-overflow: ellipsis;
+font-size: 40px;
+font-family: Roboto, sans-serif;
+font-weight: 400;
+text-align: left;
+position: absolute;
+left: 568px;
+top: 506px;
+`;
+
+export const SobregirarBtn  =styled(LinkR)`
+color: rgba(255, 255, 255, 1);
+text-overflow: ellipsis;
+font-size: 40px;
+font-family: Roboto, sans-serif;
+font-weight: 400;
+text-align: left;
+position: absolute;
+left: 664px;
+top: 605px;
+`;
+
+export const Salir =styled(LinkR)`
+	color: rgba(255, 255, 255, 1);
+	text-overflow: ellipsis;
+	font-size: 40px;
+	font-family: Roboto, sans-serif;
+	font-weight: 400;
+	text-align: left;
+	position: absolute;
+	left: 718px;
+	top: 697px;
+`;
