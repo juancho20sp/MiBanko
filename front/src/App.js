4dfc28c1d220e043f43c7869e68ce6f14d1266cd
JCPosso <juancamiloposso01@gmail.com>
2022-02-24 00:26:45 -0500

    add Routes

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Login from "./pages/login";
+import HomeUsuario from "./pages/homeUsuario";
+import CrearCuenta from "./pages/crearUsuario";
 
 function App() {
   return (
-    <div className="App">
-        <a>
-          Holi
-        </a>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login/>} />
+          <Route path="/homeUsuario" element={<HomeUsuario/>} />
+          <Route path="/crearCuenta" element={<CrearCuenta/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
