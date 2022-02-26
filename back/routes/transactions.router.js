931ea0e5ddf0fc944dfe38513cb88607e687f48e
unknown <richard.urrea@labinfo.is.escuelaing.edu.co>
2022-02-26 09:23:16 -0500

    transactions

--- back/routes/transactions.router.js
+++ back/routes/transactions.router.js
@@ -5,6 +5,21 @@ const TransctionsService = require('../services/transactions.services');
 const router = express.Router();
 const service = new TransctionsService();
 
+
+router.post('/createTransactionInter', async (req, res) => {
+  try {
+    const data = req.body.transactioninter;
+    const transactioninterData = await service.createTransactionInter(data);
+
+    res.status(200).json(transactioninterData);
+  } catch(err) {
+    res.status(500).json({
+      message: 'Something went wrong on the server'
+    })
+  }
+})
+
+
 router.get('/getTransactionsInfo', async(req, res) => {
   try {
     const result = await service.getTransactionsInfo();
--- back/services/transactions.services.js
+++ back/services/transactions.services.js
@@ -10,6 +10,66 @@ const {
 
 class TransctionsService{
 
+  async createTransactionInter(transactioninterData) {
+    // Create DB connection
+    const db = new Client(dbClient);
+    let result;
+    let check;
+
+    try {
+      await db.connect();
+
+      const {
+        destiny_account,
+        source_acc,
+        amount,
+        tdoc,
+        ndoc,
+        overdraw,
+        amount_overdraw
+      } = transactioninterData;
+
+
+      const creationDate = new Date().toISOString().slice(0, 10);
+      status_overdraw = null;
+      check = await db.query(`SELECT COUNT(*) FROM db_transactions WHERE USR_DOCTYPE=$1 AND USR_NUMDOC=$2 AND ACC_NUMBER=$3 RETURNING *`, [tdoc,
+        ndoc,
+        destiny_account
+      ])
+
+      check= check.rows[0];
+      console.log(check);
+      if(check==1){
+        
+        if(overdraw){
+          const status ="PENDIENTE";
+          await db.query(`INSERT INTO DB_TRANSACTIONS_INTRA(acc_number, ovd_creation_date, ovd_is_authorized, amount) VALUES($1, $2, $3, $4) RETURNING *`, [applicant_account,
+            creationDate,
+            status_overdraw,
+            amount
+          ]);
+          await db.query(`INSERT INTO db_overdraws(acc_number, ovd_creation_date, ovd_is_authorized, amount) VALUES($1, $2, $3, $4) RETURNING *`, [applicant_account,
+            creationDate,
+            status_overdraw,
+            amount
+          ]);
+        }
+      }
+      
+
+      result = result.rows[0];
+
+    } catch(err) {
+      result = {
+        message: 'Something went wrong'
+      }
+    } finally {
+      await db.end();
+    }
+
+    return result;
+  }
+
   async getTransactionsInfo() {
     // Create DB connection
     const db = new Client(dbClient);
