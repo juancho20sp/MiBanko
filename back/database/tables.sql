e8f24aeb815c9cbc5197aa2a49d00846f45aeda4
Ernesto Camacho <ernesto.camachoa9805gmail.com>
2022-02-25 21:56:37 -0500

    Modificacion de tables.sql fks.sql xTables.sql para que refleje el estdo de la base de datos

--- back/database/tables.sql
+++ back/database/tables.sql
@@ -3,12 +3,12 @@
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS DB_USERS(
     USR_NUMDOC                    	INTEGER     	NOT NULL, -- TODO -> Crear un check para que inicie con ("CC", "TI", "CE", "PS") (puede ser en el back)
-	USR_DOCTYPE						VARCHAR(4)		NOT NULL,
     USR_NAME                        VARCHAR(100)    NOT NULL,
     USR_LASTNAME                    VARCHAR(100)    NOT NULL,
     USR_ROLE                        VARCHAR(50)     NOT NULL, -- TODO -> Crear un check para este tipo de dato ("USER", "ADMIN", "AUDITOR")
     USR_BIRTHDATE                   DATE            NOT NULL,
-    USR_CREATION_DATE               DATE            NOT NULL
+    USR_CREATION_DATE               DATE            NOT NULL,
+    USR_DOCTYPE						VARCHAR(4)		NOT NULL
 );
 
 
@@ -16,11 +16,11 @@ CREATE TABLE IF NOT EXISTS DB_USERS(
 -- Table `DB_LOGIN`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS DB_LOGIN(
-    USR_NUMDOC                    	INTEGER     	NOT NULL, -- TODO -> Crear un check para que inicie con ("CC", "TI", "CE", "PS") (puede ser en el back)
 	UR_DOCTYPE				        VARCHAR(4)		NOT NULL,
     USR_USERNAME                    VARCHAR(75)     NOT NULL,
     USR_EMAIL                       VARCHAR(150)    NOT NULL,
-    USR_PASSWORD                    VARCHAR(300)    NOT NULL
+    USR_PASSWORD                    VARCHAR(300)    NOT NULL,
+    USR_NUMDOC                    	INTEGER     	NOT NULL  -- TODO -> Crear un check para que inicie con ("CC", "TI", "CE", "PS") (puede ser en el back)
 );
 
 
@@ -36,6 +36,24 @@ CREATE TABLE IF NOT EXISTS DB_TRANSACTIONS(
     AMOUNT                DOUBLE PRECISION    NOT NULL
 );
 
+
+-- -----------------------------------------------------
+-- Table `DB_TRANSACTIONS`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS DB_TRANSACTIONS_INTER (
+    TR_ID                           SERIAL              NOT NULL,
+    TR_DATE                         DATE                NOT NULL,
+    TR_DESTINY_BANK                 INTEGER             NOT NULL,
+    TR_DESTINY_ACCOUNT              INTEGER	            NOT NULL,
+    TR_SOURCE_ACCOUNT               INTEGER             NOT NULL,
+    TR_DESTINY_RECIVER_NAME         VARCHAR(100)        NOT NULL,
+    TR_DESTINY_RECIVER_LASTNAME     VARCHAR(100)        NOT NULL,
+    TR_DESTINY_RECIVER_TYPEDOC      VARCHAR(4)          NOT NULL,
+    TR_DESTINY_RECIVER_DOCNUM       INTEGER             NOT NULL,
+    AMOUNT                          DOUBLE PRECISION    NOT NULL
+    
+);
+
 -- -----------------------------------------------------
 -- Table `DB_ACCOUNTS`
 -- -----------------------------------------------------
@@ -45,8 +63,8 @@ CREATE TABLE IF NOT EXISTS DB_ACCOUNTS(
     ACC_CREATION_DATE      	DATE                NOT NULL,
     ACC_BALANCE            	DOUBLE PRECISION    NOT NULL,
     ACC_TYPE               	VARCHAR(75)         NOT NULL, -- TODO -> Crear un check para este tipo de dato ('AHORROS', 'CORRIENTE')
-    USR_NUMDOC              INTEGER     		NOT NULL, -- TODO -> Crear un check para que inicie con ("CC", "TI", "CE", "PS") (puede ser en el back)
     USR_DOCTYPE				VARCHAR(4)	        NOT NULL,
+    USR_NUMDOC              INTEGER     		NOT NULL  -- TODO -> Crear un check para que inicie con ("CC", "TI", "CE", "PS") (puede ser en el back)
 );
 
 -- -----------------------------------------------------
@@ -56,7 +74,8 @@ CREATE TABLE IF NOT EXISTS DB_OVERDRAWS(
     OVD_ID                 SERIAL               PRIMARY KEY,
     ACC_NUMBER             INTEGER		        NOT NULL,
     OVD_CREATION_DATE      DATE                 NOT NULL,
-    OVD_IS_AUTHORIZED      BOOLEAN              NOT NULL -- TODO -> Crear un trigger porque cuando se cree un overdraw debe crearse como NO autorizado (también se puede hacer en el back)
+    OVD_IS_AUTHORIZED      BOOLEAN              NOT NULL, -- TODO -> Crear un trigger porque cuando se cree un overdraw debe crearse como NO autorizado (también se puede hacer en el back)
+    AMOUNT                 INTEGER              NOT NULL
 );
 
 -- -----------------------------------------------------
